33fc1e8205f16b36b78225ece536dba3fb3f1a2f
Somya Jha <sumicode.exe@gmail.com>
2024-06-25 23:21:02 +0530

    changes

--- pendulum.py
+++ pendulum.py
@@ -1,1 +1,22 @@
 from __future__ import print_function
+from scipy.integrate import odeint
+
+import time
+import math
+import numpy as np
+import pylab as np
+
+#we import matplotlib.pyplot as plt
+from matplotlib import animation, rc
+from IPython.display import HTML
+from matplotlib import pyplot as plt
+
+#mass of bob for pendulum 1 and pendulum 2 (in kg)
+m1 = 5
+m2 = 2
+
+#length of string for pendulum 1 and oendulum 2 (in meters)
+l2 = 1.4
+
+#gravitational acceleration constant (in m/s^2)
+g = 9.8
